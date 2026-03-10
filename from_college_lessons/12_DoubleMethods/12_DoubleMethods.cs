034cd8667990283789f7989ac44ee8c4882b68bc
everidiy <itusitas@gmail.com>
2026-03-10 23:18:42 +0300

    Add files via upload

---
+++ from_college_lessons/12_DoubleMethods/12_DoubleMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DoubleMethods
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write($" Введите сторону равностороннего треугольника: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine($" Его площадь - {EquilateralTriangleArea(a)}\n");
+
+            Console.WriteLine($" Площадь правильного шестиугольника - {EquilateralTriangleArea(a)} * 6 = {EquilateralTriangleArea(a) * 6}");
+        }
+
+        static double EquilateralTriangleArea(double side)
+        {
+            return (Math.Sqrt(3) / 4) * Math.Pow(side, 2);
+        }
+    }
+}
