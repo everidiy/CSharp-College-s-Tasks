1569d6dc1fe8269e1f362b283c0a258227f9c89b
everidiy <itusitas@gmail.com>
2026-03-10 23:33:43 +0300

    Add files via upload

---
+++ from_college_lessons/13_Recursive_Methods/RecursiveMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RecursiveMethods
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+        static void Main()
+        {
+            int A = rnd.Next(-10, 10);
+            int B = rnd.Next(-10, 10);
+
+            Console.WriteLine($" Число А = {A}");
+            Console.WriteLine($" Число B = {B}");
+            Console.WriteLine();
+
+            if (A < B)
+            {
+                Console.Write(" Возрастание: ");
+                ToHigh(A, B);
+                Console.WriteLine();
+            } else
+            {
+                Console.Write(" Убывание: ");
+                ToLow(A, B);
+                Console.WriteLine();
+            }
+        }
+
+        static void ToHigh(int a, int b)
+        {
+            if (a > b) return;
+
+            Console.Write($" {a} ");
+
+            ToHigh(a + 1, b);
+        }
+
+        static void ToLow(int a, int b)
+        {
+            if (a < b) return;
+
+            Console.Write($" {a} ");
+
+            ToLow(a - 1, b);
+        }
+    }
+}
