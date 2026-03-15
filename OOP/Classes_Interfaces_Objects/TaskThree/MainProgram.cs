644bcf8201226baac1ac49143effd4179be097eb
everidiy <itusitas@gmail.com>
2026-03-15 18:22:27 +0300

    Add files via upload

---
+++ OOP/Classes_Interfaces_Objects/TaskThree/MainProgram.cs
@@ -0,0 +1,130 @@
+﻿
+using System;
+using TaskThree;
+
+namespace TaskOne
+{
+    internal class MainProgram
+    {
+        static Reservation reservation = new Reservation
+        {
+            name = "",
+            status = false,
+            type = "",
+        };
+
+        static void Main()
+        {
+            Console.Clear();
+
+            Console.WriteLine("\nПриветствую вас на платформе!\n");
+            Console.WriteLine("Что вас интересует?\n");
+            Console.WriteLine("Сделать бронь - (1)");
+            Console.WriteLine("Выход - (2)\n");
+
+            Console.Write("Ваш ответ => ");
+            int.TryParse(Console.ReadLine(), out int step);
+
+            switch (step)
+            {
+                case 1:
+                    AddBooking(reservation);
+                    break;
+                case 2:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Main();
+                    break;
+            }
+        }
+
+        static void AddBooking(Reservation className)
+        {
+            Console.Clear();
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Что вы хотите забронировать? \n");
+
+                Console.WriteLine("Рейс - (1) ");
+                Console.WriteLine("Отель - (2) ");
+                Console.WriteLine("Выход - (3)\n");
+
+                Console.Write("Ваш ответ => ");
+                int.TryParse(Console.ReadLine(), out int step);
+
+                switch (step)
+                {
+                    case 1:
+                        className.type = "Рейс";
+
+                        Console.Clear();
+
+                        Console.Write("Введите название рейса - ");
+                        string ticket = Console.ReadLine();
+
+                        className.name = ticket;
+
+                        ReservationFunc(className);
+                        break;
+                    case 2:
+                        className.type = "Отель";
+
+                        Console.Clear();
+
+                        Console.Write("Введите название отеля - ");
+                        string hotel = Console.ReadLine();
+
+                        className.name = hotel;
+
+                        ReservationFunc(className);
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор!");
+                        break;
+                }
+            }
+        }
+
+        static void ReservationFunc(Reservation className)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Что вас интересует?\n");
+
+                Console.WriteLine("Забронировать/Убрать бронь - (1)");
+                Console.WriteLine("Просмотреть данные брони - (2)\n");
+
+                Console.Write("Ваш ответ => ");
+                int step = Convert.ToInt32(Console.ReadLine());
+
+                switch (step)
+                {
+                    case 1:
+                        className.GetCancelBooking();
+                        Console.WriteLine("\nНажмите любую клавишу, чтобы выйти");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        className.GetInfo();
+                        Console.WriteLine("\nНажмите любую клавишу, чтобы выйти");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ OOP/Classes_Interfaces_Objects/TaskThree/Reservation.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskThree
+{
+    internal class Reservation
+    {
+        private static readonly Random rnd = new Random();
+
+        internal string name;
+        internal bool status;
+        internal string type;
+        internal string date;
+
+        public void GetCancelBooking()
+        {
+            Console.Clear();
+
+            if (status)
+            {
+                status = false;
+                Console.WriteLine("Ваша бронь была отклонена! ");
+            } else
+            {
+                Console.WriteLine("Выберите интересующую вас дату: \n");
+
+                string firstDate = GetDate();
+                string secondDate = GetDate();
+                string thirdDate = GetDate();
+
+                Console.WriteLine($"{firstDate} - (1) ");
+                Console.WriteLine($"{secondDate} - (2) ");
+                Console.WriteLine($"{thirdDate} - (3) \n");
+
+                Console.Write("Ваш ответ => ");
+                int.TryParse(Console.ReadLine(), out int step);
+                
+                switch (step)
+                {
+                    case 1:
+                        status = true;
+                        date = firstDate;
+                        Console.WriteLine("\nВаша бронь была выполнена! ");
+                        break;
+                    case 2:
+                        status = true;
+                        date = secondDate;
+                        Console.WriteLine("\nВаша бронь была выполнена! ");
+                        break;
+                    case 3:
+                        status = true;
+                        date = thirdDate;
+                        Console.WriteLine("\nВаша бронь была выполнена! ");
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор!");
+                        break;
+                }
+            }
+        }
+
+        public string GetDate()
+        {
+
+            int month = rnd.Next(1, 13);
+            int day = 0;
+
+            if (month == 2)
+            {
+                day = rnd.Next(1, 29);
+            }
+            else
+            {
+                day = rnd.Next(1, 32);
+            }
+
+            int year = 2026;
+
+            string fullDate = $"{day:D2}.{month:D2}.{year}";
+
+            return fullDate;
+        }
+
+        public void GetInfo()
+        {
+            string isActive = status ? "Забронировано" : "Не забронировано";
+            string isHotel = type.ToString().ToLower() == "отель" ? "отеля" : "рейса";
+
+            Console.Clear();
+
+            Console.WriteLine($"\nВаше бронирование - {type}: {name}");
+            Console.WriteLine($"Название {isHotel} - {name}");
+            Console.WriteLine($"Тип брони - {type}");
+            Console.WriteLine($"Статус бронирования - {isActive}");
+            Console.WriteLine($"Запланировано на - {date}\n");
+        }
+    }
+}
