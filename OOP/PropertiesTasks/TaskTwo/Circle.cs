7db3c033a6891bf0948dca332401fccb321037bd
everidiy <itusitas@gmail.com>
2026-03-17 17:18:50 +0300

    Add files via upload

---
+++ OOP/PropertiesTasks/TaskTwo/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TaskTwoProperties
+{
+    internal class Circle
+    {
+        //поле класса
+        private double _radius;
+
+        //Главное свойство (с логикой!)
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("\nРадиус должен быть больше нуля\n");
+
+                _radius = value;
+            }
+        }
+
+        //конструктор
+        public Circle(double radius)
+        {
+            Radius = radius; // используем свойство (с проверкой)
+        }
+
+        //Вычисляемые свойства
+        public double Area => Math.PI * Radius * Radius;
+        public double CircleLength => 2 * Math.PI * Radius;
+
+    }
+}
---
+++ OOP/PropertiesTasks/TaskTwo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace TaskTwoProperties
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Задайте радиус вашему кругу: ");
+            double.TryParse(Console.ReadLine(), out double rds);
+
+            Circle circle = new Circle(rds);
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Площадь круга - (1)");
+                Console.WriteLine("Длина окружности - (2)");
+                Console.WriteLine("Выход - (3)\n");
+
+                Console.WriteLine("Какая функция вас интересует?");
+                int.TryParse(Console.ReadLine(), out int func);
+
+                switch (func)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine($"Площадь вашего круга с радиусом - {circle.Radius} = {circle.Area}");
+                        Console.WriteLine("\nНажмите любую клавишу, чтобы вернуться в меню..");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine($"Длина окружности вашего круга с радиусом - {circle.Radius} = {circle.CircleLength}");
+                        Console.WriteLine("\nНажмите любую клавишу, чтобы вернуться в меню..");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        exit = true; 
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Неверный выбор!");
+                        Thread.Sleep(1000);
+                        break;
+                }
+            }
+        }
+    }
+}
