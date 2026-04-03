460302a4c4c7c7b1142cae9285d00fa988666c78
everidiy <itusitas@gmail.com>
2026-04-04 00:19:21 +0300

    Add files via upload

---
+++ WPF/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        private double firstNum = 0;
+        private string operation = "";
+        private double SecondNum = 0;
+        private bool isNewNumber = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "0";
+            firstNum = 0;
+            operation = "";
+            SecondNum = 0;
+        }
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (isNewNumber)
+            {
+                Display.Text = btn.Content.ToString();
+                isNewNumber = false;
+            }
+            else
+            {
+                Display.Text += btn.Content.ToString();
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            firstNum = double.Parse(Display.Text);
+            operation = btn.Content.ToString();
+            isNewNumber = true;
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            SecondNum = double.Parse(Display.Text);
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+": result = firstNum + SecondNum; break;
+                case "-": result = firstNum - SecondNum; break;
+                case "*": result = firstNum * SecondNum; break;
+                case "/": result = firstNum / SecondNum; break;
+
+                case "^": result = Math.Pow(firstNum, SecondNum); break;
+                case "%": result = firstNum % SecondNum; break;
+
+                case "Sin": result = Math.Sin(firstNum); break;
+                case "Cos": result = Math.Cos(firstNum); break;
+                case "Sqrt": result = Math.Sqrt(firstNum); break;
+            }
+
+            Display.Text = result.ToString("0.########");
+            isNewNumber = true;
+        }
+
+        private void Comma_Click(object sender, RoutedEventArgs e)
+        {
+            if (Display.Text.Contains(",")) return;
+
+            Button btn = (Button)sender;
+
+            Display.Text += btn.Content.ToString();
+        }
+    }
+}
---
+++ WPF/Converter/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Windows;
+
+namespace ConverterApp
+{
+    public partial class MainWindow : Window
+    {
+        private string[] currencies;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string apiKey = "b384bbfef1edbfc27b335b9f";
+            string url = $"https://v6.exchangerate-api.com/v6/{apiKey}/latest/USD";
+
+            using HttpClient client = new HttpClient();
+
+            try
+            {
+                string response = await client.GetStringAsync(url);
+                var data = JObject.Parse(response);
+
+                var rates = data["conversion_rates"] as JObject;
+                currencies = rates.Properties().Select(p => p.Name).ToArray();
+
+                comboFrom.ItemsSource = currencies;
+                comboTo.ItemsSource = currencies;
+
+                comboFrom.SelectedIndex = 0;
+                comboTo.SelectedIndex = 1;
+            } 
+            catch
+            {
+                MessageBox.Show("Ошибка при получении данных валют");
+            }
+        }
+
+        private async void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(txtAmount.Text, out double amount))
+            {
+                MessageBox.Show("Введите число!");
+                return;
+            }
+
+            string apiKey = "b384bbfef1edbfc27b335b9f";
+
+            string from = comboFrom.SelectedItem.ToString();
+            string to = comboTo.SelectedItem.ToString();
+
+            string url = $"https://v6.exchangerate-api.com/v6/{apiKey}/latest/{from}";
+
+            using HttpClient client = new HttpClient();
+
+            try
+            {
+                string response = await client.GetStringAsync(url);
+                var data = JObject.Parse(response);
+
+                double rate = (double)data["conversion_rates"][to];
+                double result = amount * rate;
+
+                txtResult.Text = $"{amount} {from} = {result} {to}";
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при получении данных");
+            }
+        }
+    }
+}
