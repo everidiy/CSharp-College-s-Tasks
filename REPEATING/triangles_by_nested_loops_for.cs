f977e79420456e58612490437d97a42e0b40e866
everidiy <itusitas@gmail.com>
2026-04-03 22:48:48 +0300

    Add files via upload

---
+++ REPEATING/triangles_by_nested_loops_for.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace repeating
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 10; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 10; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 10; i > 0; i--)
+            {
+                for (int j = 10; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
